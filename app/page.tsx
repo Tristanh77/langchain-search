23227b7f06cfb9d14fd7a928cd3f440267def7c7
tristanH77 <tristanhartman77@gmail.com>
2023-09-13 14:16:36 -0400

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -35,17 +35,42 @@ export default function Home() {
       setLoading(false)
     }
   }
+
   return (
-    <main className="flex flex-col items-center justify-between p-10">
-      <input className='text-black px-2 py-1' onChange={e => setQuery(e.target.value)} />
-      <button className="px-7 py-1 rounded-3xl bg-white text-black mt-2 mb-2" onClick={sendQuery}>Ask</button>
-      {
-        loading && <p>Asking AI ...</p>
-      }
-      {
-        result && <p>{result}</p>
-      }
-      <button onClick={createIndexAndEmbeddings}>Load File </button>
-    </main>
-  )
+    <main
+      className="flex flex-col items-center justify-center h-screen"
+      style={{
+        backgroundImage: 'linear-gradient(45deg, #fc466b, #3f5efb)',
+      }}
+    >
+      <div className="max-w-md p-6 mx-auto bg-white rounded-lg shadow-lg">
+        <h1 className="text-3xl font-semibold text-center">Ask the AI</h1>
+        <input
+          className="w-full px-4 py-2 mt-4 border rounded-lg focus:outline-none focus:border-blue-500"
+          type="text"
+          placeholder="Your question..."
+          onChange={(e) => setQuery(e.target.value)}
+          style={{ color: 'black' }} 
+        />
+        <button
+          className="w-full mt-4 px-6 py-3 text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none"
+          onClick={sendQuery}
+        >
+          Ask
+        </button>
+        {loading && <p className="mt-4 text-center">Asking AI...</p>}
+        {result && (
+          <div className="mt-4 p-4 bg-gray-200 rounded-lg">
+            <p className="text-lg">{result}</p>
+          </div>
+        )}        
+        <button
+        className="w-full mt-4 px-6 py-3 text-white bg-green-500 rounded-lg hover:bg-green-600 focus:outline-none"
+        onClick={createIndexAndEmbeddings}
+      >
+        Load File
+      </button>
+    </div>
+  </main>
+);
 }
